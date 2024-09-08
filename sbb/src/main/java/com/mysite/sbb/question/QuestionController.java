1d464a696649cbbff596affbf40d799c9540fe94
이호룡 <kmn0848@naver.com>
2024-09-08 21:38:04 +0900

    댓글 갯수 표시

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,8 +1,6 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
